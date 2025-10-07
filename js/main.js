4d7eb304a8f3a27115750b1ab00e0b65b7ae24ef
towomey19 <sarahmayer365@gmail.com>
2025-10-07 10:58:37 -0500

    javascript button

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 // JS scripts placed here
+const blueButton = document.getElementById('blue-bg');
+const body = document.querySelector('body');
+
+blueButton.addEventListener('click', () => {
+    body.classList.toggle('blue');
+});
